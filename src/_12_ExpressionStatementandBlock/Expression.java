4db5fa3d8a6b481e4af468fb27694c19fc70a1e7
Permana <sdharma721@gmail.com>
2026-03-03 04:47:29 +0700

    `feat: exp-stmt (Expression): add description for Expression Statement`

--- src/_12_ExpressionStatementandBlock/Expression.java
+++ src/_12_ExpressionStatementandBlock/Expression.java
@@ -2,6 +2,7 @@ package _12_ExpressionStatementandBlock;
 /**
  * Expression adalah konstruksi dari variabel, operator dan pemanggilan method yang mengevaluasi menjadi sebuah single value
  * Expression adalah core component dari statement
+ * Expression statement adalah statement yang terdiri dari expression, seperti assignment, method invocation, increment/decrement, dan object creation
  */
 public class Expression {
     static void main(String[] args) {
